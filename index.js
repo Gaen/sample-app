1b2825164a8008a8f1a8f2ccaa6116a056562494
Mikhail Gaenkov <gaen.wrk@gmail.com>
2023-02-24 19:36:27 +0600

    add logging with pino-http + pino-pretty

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const pino = require('pino-http');
+const pretty = require('pino-pretty');
+
 const app = express();
 const port = 3000;
+
+app.use(pino(pretty()));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
