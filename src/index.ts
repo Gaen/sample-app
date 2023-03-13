3deb3ee65204188b52f9de6bbae4305fbfa1a552
Mikhail Gaenkov <gaen.wrk@gmail.com>
2023-03-13 21:03:38 +0600

    introduce typescript

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import {Request, Response} from 'express';
+
 const express = require('express');
 const pino = require('pino-http');
 const pretty = require('pino-pretty');
@@ -8,7 +10,7 @@ const app = express();
 
 app.use(pino(pretty({destination: process.stdout})));
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
     console.log('Got request!');
     res.send('Hello World! This is new version!');
 });
